3f3b40fd41ca2629c9afed5de1282493d09e2392
Vatai <Vataioff@gmail.com>
2026-03-20 14:57:34 +1100

    Удалены неиспользуемые файлы (38 файлов)
    Frontend (3):
    - Vacation.tsx.backup, NotificationLoader.tsx, DocumentsSection.tsx
    
    Backend - одноразовые миграции (17):
    - Все migrate-*.js файлы (функционал в основном migrate.js)
    
    Backend - ad-hoc тесты/утилиты (15):
    - Тестовые скрипты не из package.json
    - Утилиты для одноразовых задач
    
    Backend - SQL схемы (3):
    - schema*.sql файлы (документация, не используются)

--- backend/src/tests/generate-hash.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import bcrypt from 'bcryptjs'
-
-const password = 'TestPassword123!'
-const hash = bcrypt.hashSync(password, 10)
-
-console.log('Password:', password)
-console.log('Hash:', hash)
