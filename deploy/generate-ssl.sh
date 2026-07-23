52d5f79b1734ef84a4fac8cb7f71023094f55f0a
Vatai <Vataioff@gmail.com>
2026-07-23 15:19:19 +1100

    cleanup: remove unused docs, deploy scripts, specs

--- deploy/generate-ssl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p deploy/ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout deploy/ssl/server.key \
-  -out deploy/ssl/server.crt \
-  -subj "/CN=localhost"
